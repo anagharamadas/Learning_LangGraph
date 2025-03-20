e2c8699cc2ca55cd11f78b7fa8b82cb3296fdcda
Anagha Ramadas Mulloth <55655339+anagharamadas@users.noreply.github.com>
2025-03-20 06:07:35 +0530

    created_a_basic_StateGraph

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langchain_anthropic import ChatAnthropic
+from IPython.display import Image, display
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
 
 def _set_env(var:str):
     if not os.environ.get(var):
@@ -15,3 +22,27 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 graph_builder = StateGraph(State)
+
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+def chatbot(state: State):
+    return {"messages" : [llm.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+
+graph_builder.add_edge(START, "chatbot")
+
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+try:
+    graph_image = graph.get_graph().draw_mermaid_png()
+    with open("graph.png", "wb") as f:
+        f.write(graph_image)
+    print("Graph saved as 'graph.png'. Open this file to view the graph.")
+except Exception:
+    print(f"An error occurred: {e}")
